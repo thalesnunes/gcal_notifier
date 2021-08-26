bbb8cbbfed28359c770f1bdefceccda88fbd1c0b
Thales Nunes <thalesaknunes22@gmail.com>
2021-08-25 23:52:42 -0300

    Added python tags

--- setup.py
+++ setup.py
@@ -31,7 +31,12 @@ setup(
     author_email="thalesaknunes22@gmail.com",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
