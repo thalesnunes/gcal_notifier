57dd6a291a2fa9db3b198fcf6adaac472062cf7e
Thales Nunes <thalesaknunes22@gmail.com>
2022-01-25 00:17:10 -0300

    feat: using new print_events

--- gcal_notifier/main.py
+++ gcal_notifier/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from datetime import datetime
-from pathlib import Path
 from typing import Any, Dict, NoReturn, Tuple
 
 from gcal_notifier.cli import cli
@@ -9,7 +8,7 @@ from gcal_notifier.event_getter import SimpleGCalendarGetter
 from gcal_notifier.event_loader import load_saved_events
 from gcal_notifier.event_printer import SimpleGCalendarPrinter
 from gcal_notifier.event_reminder import SimpleGCalendarNotifier
-from gcal_notifier.event_saver import save_events
+from gcal_notifier.event_saver import save_events, transform_events
 from gcal_notifier.globals import CACHE
 from gcal_notifier.utils import define_period
 
@@ -18,7 +17,6 @@ def run_getter(
     general_params: Dict[str, Any],
     calendar_params: Dict[str, Any],
     period: Tuple[datetime, datetime],
-    save_path: Path = CACHE / "events_notify.json"
 ) -> NoReturn:
     """Run SimpleGCalendarGetter with user configs.
 
@@ -27,10 +25,10 @@ def run_getter(
         calendar_params (Dict[str, Any]): Calendar params
         save_path (str): Path to file to be saved
     """
-    getter = SimpleGCalendarGetter(general_params, calendar_params, period)
-    getter.load_calendars()
+    getter = SimpleGCalendarGetter(general_params, calendar_params)
+    getter.load_calendars(period)
     getter.load_events()
-    save_events(getter.events, file_path=save_path)
+    save_events(getter.events, file_path=CACHE/"events_notify.json")
 
 
 def run_notifier(
@@ -43,7 +41,10 @@ def run_notifier(
         calendar_params (Dict[str, Any]): Calendar params
     """
     saved_events = load_saved_events()
-    SimpleGCalendarNotifier(saved_events, general_params, calendar_params)
+    notifier = SimpleGCalendarNotifier(
+        saved_events, general_params, calendar_params
+    )
+    notifier.search_reminders()
 
 
 def run_printer(
@@ -59,9 +60,12 @@ def run_printer(
         calendar_params (Dict[str, Any]): Calendar params
         format (str): Format to use when printing events
     """
-    saved_events = load_saved_events()
+    getter = SimpleGCalendarGetter(general_params, calendar_params)
+    getter.load_calendars(period)
+    getter.load_events()
+    events_to_print = transform_events(getter.events)
     printer = SimpleGCalendarPrinter(
-        saved_events, general_params, calendar_params, period, format=format
+        events_to_print, general_params, calendar_params, period, format=format
     )
     printer.print_events(format)
 
